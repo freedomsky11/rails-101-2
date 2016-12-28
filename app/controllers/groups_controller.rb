72e382076aea6924f3437380c636a23ce166eaf2
freedomsky <hanwei_1989@126.com>
2016-12-28 21:38:48 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permisson, only: [:edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -43,11 +43,11 @@ class GroupsController < ApplicationController
 
   private
 
-  def find_group_and_check_permisson
+  def find_group_and_check_permission
     @group = Group.find(params[:id])
 
     if current_user != @group.user
-      redirect_to root_path, alert: "You have no permisson."
+      redirect_to root_path, alert: "You have no permission."
     end
   end
 
